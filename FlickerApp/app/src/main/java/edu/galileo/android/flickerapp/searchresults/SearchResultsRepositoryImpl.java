4896a24b9d3190e35be39c605c6b335b1af091c1
sofylopdev <sofylopdev@gmail.com>
2018-09-04 17:06:04 +0100

    feat: implemented repository

---
+++ FlickerApp/app/src/main/java/edu/galileo/android/flickerapp/searchresults/SearchResultsRepositoryImpl.java
@@ -0,0 +1,108 @@
+package edu.galileo.android.flickerapp.searchresults;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.galileo.android.flickerapp.api.FlickrService;
+import edu.galileo.android.flickerapp.api.entities.Photo;
+import edu.galileo.android.flickerapp.api.entities.PhotosResponse;
+import edu.galileo.android.flickerapp.entities.Picture;
+import edu.galileo.android.flickerapp.libs.MyEventBus;
+import edu.galileo.android.flickerapp.searchresults.events.SearchResultsEvent;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SearchResultsRepositoryImpl implements SearchResultsRepository {
+
+    public static final int RESULTS_PER_PAGE = 15;
+
+    private MyEventBus eventBus;
+    private FlickrService service;
+
+    private List<Picture> pictureList;
+
+    public SearchResultsRepositoryImpl(MyEventBus eventBus, FlickrService service) {
+        this.eventBus = eventBus;
+        this.service = service;
+        pictureList = new ArrayList<>();
+    }
+
+    @Override
+    public void loadPictures(String tags) {
+        Call<PhotosResponse> call = service.search(tags, RESULTS_PER_PAGE);
+        call.enqueue(new Callback<PhotosResponse>() {
+            @Override
+            public void onResponse(Call<PhotosResponse> call, Response<PhotosResponse> response) {
+                Log.d("Repository", "Got response");
+                if (response.isSuccessful()) {
+
+                    PhotosResponse photosResponse = response.body();
+                    if (photosResponse.getPhotos() != null) {
+                        List<Photo> photoList = photosResponse.getPhotos().getPhoto();
+
+                        if (photoList != null && !photoList.isEmpty()) {
+                            for (Photo photo : photoList) {
+                                Picture picture = new Picture();
+                                picture.setTitle(photo.getTitle());
+                                String pictureUrl = buildPictureUrl(
+                                        photo.getFarm(),
+                                        photo.getServer(),
+                                        photo.getId(),
+                                        photo.getSecret());
+                                picture.setImageURL(pictureUrl);
+                                pictureList.add(picture);
+                            }
+                        }
+                    }
+
+                    post(pictureList.get(0), null, SearchResultsEvent.GET_NEXT_EVENT);
+                } else {
+                    post(null, response.message(), SearchResultsEvent.ERROR_EVENT);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PhotosResponse> call, Throwable t) {
+                post(null, t.getLocalizedMessage(), SearchResultsEvent.ERROR_EVENT);
+            }
+        });
+    }
+
+    @Override
+    public void getNextPicture() {
+        pictureList.remove(0);
+        post(pictureList.get(0), null, SearchResultsEvent.GET_NEXT_EVENT);
+    }
+
+    @Override
+    public void savePicture(Picture picture) {
+        picture.save();
+        post(null, null, SearchResultsEvent.SAVE_EVENT);
+    }
+
+    private void post(Picture picture, String errorMsg, int type) {
+        SearchResultsEvent event = new SearchResultsEvent();
+        event.setErrorMsg(errorMsg);
+        event.setPicture(picture);
+        event.setType(type);
+        eventBus.post(event);
+    }
+
+    private String buildPictureUrl(int farmId, String serverId, String pictureId, String secret) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("https://farm");
+        builder.append(farmId);
+        builder.append(".staticflickr.com/");
+        builder.append(serverId);
+        builder.append("/");
+        builder.append(pictureId);
+        builder.append("_");
+        builder.append(secret);
+        builder.append(".jpg");
+
+        return builder.toString();
+    }
+}
--- FlickerApp/app/src/main/java/edu/galileo/android/flickerapp/searchresults/ui/SearchResultsActivity.java
+++ FlickerApp/app/src/main/java/edu/galileo/android/flickerapp/searchresults/ui/SearchResultsActivity.java
@@ -1,16 +1,37 @@
 package edu.galileo.android.flickerapp.searchresults.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import org.greenrobot.eventbus.EventBus;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import edu.galileo.android.flickerapp.R;
+import edu.galileo.android.flickerapp.api.FlickerClient;
+import edu.galileo.android.flickerapp.entities.Picture;
+import edu.galileo.android.flickerapp.libs.ImageLoader;
+import edu.galileo.android.flickerapp.libs.MyEventBus;
+import edu.galileo.android.flickerapp.searchresults.GetNextPictureInteractor;
+import edu.galileo.android.flickerapp.searchresults.GetNextPictureInteractorImpl;
+import edu.galileo.android.flickerapp.searchresults.LoadPicturesInteractorImpl;
+import edu.galileo.android.flickerapp.searchresults.SavePictureInteractorImpl;
+import edu.galileo.android.flickerapp.searchresults.SearchResultsPresenter;
+import edu.galileo.android.flickerapp.searchresults.SearchResultsPresenterImpl;
+import edu.galileo.android.flickerapp.searchresults.SearchResultsRepository;
+import edu.galileo.android.flickerapp.searchresults.SearchResultsRepositoryImpl;
 
-public class SearchResultsActivity extends AppCompatActivity {
+import static edu.galileo.android.flickerapp.main.ui.MainActivity.TAGS_EXTRA;
+
+public class SearchResultsActivity extends AppCompatActivity implements SearchResultsView {
 
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
@@ -19,10 +40,83 @@ public class SearchResultsActivity extends AppCompatActivity {
     @BindView(R.id.pictureTitle)
     TextView pictureTitle;
 
+    private SearchResultsPresenter presenter;
+    private ImageLoader imageLoader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searched_results);
         ButterKnife.bind(this);
+        setupInjection();
+        presenter.onCreate();
+
+        Intent intent = getIntent();
+        if(intent.getStringExtra(TAGS_EXTRA) != null) {
+            presenter.loadImages(intent.getStringExtra(TAGS_EXTRA));
+        }
+    }
+
+    private void setupInjection() {
+        MyEventBus eventBus = new MyEventBus(EventBus.getDefault());
+        SearchResultsRepositoryImpl repository = new SearchResultsRepositoryImpl(eventBus, new FlickerClient().getFlickrService());
+        presenter = new SearchResultsPresenterImpl(eventBus, this, new SavePictureInteractorImpl(repository), new GetNextPictureInteractorImpl(repository), new LoadPicturesInteractorImpl(repository) {
+        });
+        imageLoader = new ImageLoader(Glide.with(this));
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showUIElements() {
+        picture.setVisibility(View.VISIBLE);
+        pictureTitle.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hidUIElements() {
+        picture.setVisibility(View.GONE);
+        pictureTitle.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void saveAnimation() {
+
+    }
+
+    @Override
+    public void dismissAnimation() {
+
+    }
+
+    @Override
+    public void onPictureSaved() {
+        String savedTextToast = "Picture saved.";
+        Toast.makeText(this, savedTextToast, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setPictureAndTitle(Picture picture) {
+        imageLoader.load(this.picture, picture.getImageURL());
+        pictureTitle.setText(String.format("Title: %s", picture.getTitle()));
+    }
+
+    @Override
+    public void onPictureError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
     }
 }
